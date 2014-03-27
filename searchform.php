4a8ddc42c79f9a46edefea2936b2f03421c477ba
Bijay Yadav <bijay@idealaya.com.np>
2014-03-27 10:25:34 +0545

    search form updated

--- searchform.php
+++ searchform.php
@@ -9,8 +9,8 @@ if ( !defined('ABSPATH')) exit;
  * @since Sampression Lite 1.0
  */
 ?>
-<form method="get" class="searchform" class="clearfix" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+<form method="get" class="searchform clearfix" action="<?php echo esc_url( home_url( '/' ) ); ?>">
     <label class="hidden" for="s"><?php _e( 'Search for:', 'sampression' ); ?></label>
-    <input type="text" value="<?php the_search_query(); ?>" name="s" class="search-field" class="text-field" placeholder="<?php _e( 'search by keyword', 'sampression' ); ?>" />
+    <input type="text" value="<?php the_search_query(); ?>" name="s" class="search-field text-field" placeholder="<?php _e( 'search by keyword', 'sampression' ); ?>" />
     <input type="submit" class="searchsubmit" value="<?php _e( 'Search', 'sampression' ); ?>" />
 </form>

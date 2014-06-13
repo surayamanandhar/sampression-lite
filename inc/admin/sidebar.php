55b9e4e77a1793ae84f1def7354986ccdee7371a
Bijay Yadav <bijay@idealaya.com.np>
2014-06-13 14:54:45 +0545

    delete directory

--- inc/admin/sidebar.php
+++
@@ -1,8 +0,0 @@
-<?php if ( ! defined( 'ABSPATH' ) ) exit( 'restricted access' ); ?>
-<div id="sidebar-nav">
-    <ul class="clearfix">
-    	<?php sampression_option_menu() ?>
-
-    </ul>
-</div>
-<div class="sam-saving-info"><?php echo _e( 'Please save your changes by clicking save button at the bottom', 'sampression' ); ?></div>

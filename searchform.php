66936acbd52a10cb7b2d552a98ff823e7c85dad6
Anibesh Thapa <anibesht@gmail.com>
2016-01-19 11:05:33 +0545

    Removed images.

--- searchform.php
+++ searchform.php
@@ -11,6 +11,6 @@ if ( !defined('ABSPATH')) exit;
 ?>
 <form method="get" class="search-form clearfix" action="<?php echo esc_url( home_url( '/' ) ); ?>">
     <label class="hidden"><?php _e( 'Search for:', 'sampression' ); ?></label>
-    <input type="text" value="<?php the_search_query(); ?>" name="s" class="search-field text-field" placeholder="<?php _e( 'search by keyword', 'sampression' ); ?>" />
+    <input type="text" value="<?php the_search_query(); ?>" name="s" class="search-field text-field" placeholder="<?php _e( 'Search by Keyword', 'sampression' ); ?>" />
     <button type="submit" class="search-submit"><span class="screen-reader-text"><?php echo _x( 'Search', 'submit button', 'sampression' ); ?></span></button>
 </form>

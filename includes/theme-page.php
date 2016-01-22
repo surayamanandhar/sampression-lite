def86e31eac6705cc505160f7c00630cc053e1b0
Prayas Sapkota <sapkotaprayas@gmail.com>
2016-01-22 18:33:11 +0545

    Theme page updated

--- includes/theme-page.php
+++ includes/theme-page.php
@@ -25,23 +25,24 @@ function about_sampression_theme_page() {
     ?>
     <div class="wrap" style="width:75%">
         <div>
-            <h2>Welcome to Sampression Lite.</h2>
-            <p>Hope you will enjoy using Sampression Lite.</p>
-            <p>We want to make sure you have the best experience using Sampression Lite and that is why we gathered here all the necessary informations for you. We hope you will enjoy using Sampression Lite, as much as we enjoy creating great products.</p>
+            <h1>Welcome to Sampression Lite.</h1>
+            <p>We hope that you will enjoy using Sampression Lite.</p>
+            <p>We want to make sure that you have the best experience using this theme. We have gathered here all the necessary information to make your process easier and faster. We hope you will enjoy using Sampression Lite, as much as we enjoy creating products.</p>
         </div>
         <div>
             <h2>Getting started</h2>
             <h4>Customize everything in a single place.</h4>
-            <p>Using the WordPress Customizer you can easily customize every aspect of the theme.</p>
+            <p>Using the WordPress Customizer, you can easily customize every aspect of the theme.</p>
             <p><a class="button button-primary" href="customize.php?return=%2Fwp-admin%2Fthemes.php%3Fpage%3Dabout-sampression">Go to Customizer</a></p>
+            <p>For further help, please visit our support page at: <a href="https://support.sampression.com/hc/en-us" target="_blank">https://support.sampression.com/hc/en-us</a></p>
         </div>
         <div>
-            <h2>Sampression Pro</h2>
+            <h2>Sampression PRO</h2>
+            <p>We also have a PRO version of Sampression Lite, featuring more customizable features like multiple homepage layouts, block page layout, typography options and many more.</p>
             <p>
-                <a target="_blank" class="button button-primary" href="http://www.sampression.com/themes/sampression-pro/">Buy Now</a>
-                <a target="_blank" class="button button-primary" href="http://www.demo.sampression.com/sampression-pro/">View Demo</a>
+                <a target="_blank" class="button button-primary" href="http://www.sampression.com/themes/sampression-pro/">Upgrade to PRO</a>
+                <a target="_blank" class="button button-primary" href="http://www.demo.sampression.com/sampression-pro/">Live Theme Demo</a>
             </p>
-            <p>We want to make sure you have the best experience using Sampression Lite and that is why we gathered here all the necessary informations for you. We hope you will enjoy using Sampression Lite, as much as we enjoy creating great products.</p>
         </div>
     </div>
     <?php

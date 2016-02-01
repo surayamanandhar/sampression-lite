73ccbc8ae7c549a82cb4b9e9cd0d304cfc851d58
Prayas Sapkota <sapkotaprayas@gmail.com>
2016-02-01 12:26:26 +0545

    Visit site button added on theme page

--- includes/theme-page.php
+++ includes/theme-page.php
@@ -33,7 +33,7 @@ function about_sampression_theme_page() {
             <h2>Getting started</h2>
             <h4>Customize everything in a single place.</h4>
             <p>Using the WordPress Customizer, you can easily customize every aspect of the theme.</p>
-            <p><a class="button button-primary" href="customize.php?return=%2Fwp-admin%2Fthemes.php%3Fpage%3Dabout-sampression">Go to Customizer</a></p>
+            <p><a class="button button-primary" href="customize.php?return=%2Fwp-admin%2Fthemes.php%3Fpage%3Dabout-sampression">Go to Customizer</a> <a class="button button-primary" href="<?php bloginfo('wpurl') ?>" target="_blank">Visit <?php bloginfo('name') ?></a></p>
             <p>For further help, please visit our support page at: <a href="https://support.sampression.com/hc/en-us" target="_blank">https://support.sampression.com/hc/en-us</a></p>
         </div>
         <div>
